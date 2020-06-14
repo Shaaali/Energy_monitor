2aaf3da40cff294d9de52b5054e072e61005d764
Shalitha Pathiranage <shali96shri@gmail.com>
2020-06-15 03:40:33 +0530

    setup 1

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,19 @@
+import {React , Component} from 'react';
+
+class Home extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    Home Component
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
